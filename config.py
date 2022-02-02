4adda8d8596ad25b982af5130c2fd06c44d1ae3d
Al <714006+kamlang@users.noreply.github.com>
2022-02-02 10:42:52 +0100

    adding validators in models, adding next for login

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class DevelopmentConfig(Config):
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    APP_MAIL_SUBJECT_PREFIX = '[Glgmsh]'
+    APP_MAIL_SUBJECT_PREFIX = '[Glgmsh] '
     APP_MAIL_ADMIN = os.environ.get('MAIL_ADMIN')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'users-dev.db')
