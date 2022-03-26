a5be0a39661f02f545e074d8aae2eec9e50c160b
Al <714006+kamlang@users.noreply.github.com>
2022-03-26 19:22:18 +0100

    fixed some js

--- app/main/views.py
+++ app/main/views.py
@@ -26,30 +26,6 @@ from app.auth import auth
 from app.main import main
 from app.sse.views import push_message_to_redis
 from app import db
-from app.email import send_email
-
-
-def with_csrf_validation(view_func):
-    # Added to handle request send via xhr
-    @wraps(view_func)
-    def validating_csrf(*args, **kwargs):
-        csrf_token = request.headers.get("X-CSRFToken")
-        try:
-            validate_csrf(csrf_token)
-        except Exception as e:
-            raise e
-        return view_func(*args, **kwargs)
-
-    return validating_csrf
-
-### Event Stream TODO:Move to a different bp.
-
-
-
-
-
-### Defining view functions
-
 
 @main.route("/")
 def home():
@@ -101,46 +77,6 @@ def conversations():
         next_url=next_url,
         prev_url=prev_url,
     )
-
-
-@main.route("/conversation/<uuid:conversation_uuid>/delete")
-@login_required
-@with_csrf_validation
-def delete_conversation(conversation_uuid):
-    # Accessed via xhr to delete a conversation.
-    conversation = Conversation.get_conversation_by_uuid(conversation_uuid)
-    try:
-        conversation.delete()
-        return redirect(url_for("main.conversations"))
-    except:
-        abort(403)
-
-
-@main.route("/conversation/<uuid:conversation_uuid>/leave")
-@login_required
-@with_csrf_validation
-def leave_conversation(conversation_uuid):
-    # Accessed via xhr to leave a conversation.
-    conversation = Conversation.get_conversation_by_uuid(conversation_uuid)
-    try:
-        conversation.remove_user()
-        return redirect(url_for("main.conversations"))
-    except:
-        abort(403)
-
-
-@main.route("/conversation/<uuid:conversation_uuid>/mark_as_read")
-@login_required
-@with_csrf_validation
-def mark_as_read(conversation_uuid):
-    """Accessed via xhr to mark the conversation as read when user is currently browsing it
-    also reset unread message count"""
-    conversation = Conversation.get_conversation_by_uuid(conversation_uuid)
-    try:
-        conversation.reset_unread_messages()
-        return Response(status=204)
-    except:
-        abort(403)
 
 
 @main.route("/conversation/<uuid:conversation_uuid>", methods=["GET", "POST"])
@@ -212,6 +148,21 @@ def conversation(conversation_uuid):
         conversation=conversation,
     )
 
+### XHR views
+
+def with_csrf_validation(view_func):
+    # Added to handle request send via xhr
+    @wraps(view_func)
+    def validating_csrf(*args, **kwargs):
+        csrf_token = request.headers.get("X-CSRFToken")
+        try:
+            validate_csrf(csrf_token)
+        except Exception as e:
+            raise e
+        return view_func(*args, **kwargs)
+
+    return validating_csrf
+
 
 @main.route("/getUserInfo/<username>")
 @login_required
@@ -228,6 +179,44 @@ def get_user_info(username):
         }
     )
 
+@main.route("/conversation/<uuid:conversation_uuid>/delete")
+@login_required
+@with_csrf_validation
+def delete_conversation(conversation_uuid):
+    # Accessed via xhr to delete a conversation.
+    conversation = Conversation.get_conversation_by_uuid(conversation_uuid)
+    try:
+        conversation.delete()
+        return redirect(url_for("main.conversations"))
+    except:
+        abort(403)
+
+
+@main.route("/conversation/<uuid:conversation_uuid>/leave")
+@login_required
+@with_csrf_validation
+def leave_conversation(conversation_uuid):
+    # Accessed via xhr to leave a conversation.
+    conversation = Conversation.get_conversation_by_uuid(conversation_uuid)
+    try:
+        conversation.remove_user()
+        return redirect(url_for("main.conversations"))
+    except:
+        abort(403)
+
+
+@main.route("/conversation/<uuid:conversation_uuid>/mark_as_read")
+@login_required
+@with_csrf_validation
+def mark_as_read(conversation_uuid):
+    """Accessed via xhr to mark the conversation as read when user is currently browsing it
+    also reset unread message count"""
+    conversation = Conversation.get_conversation_by_uuid(conversation_uuid)
+    try:
+        conversation.reset_unread_messages()
+        return Response(status=204)
+    except:
+        abort(403)
 
 @main.before_request
 def before_request():
